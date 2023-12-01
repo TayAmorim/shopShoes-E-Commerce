9c2fb86948a6327f222cfc3928df8093ae06b9a1
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-12-01 17:38:40 -0300

    :sparkles: feat: react-router e menu ativo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,25 +3,30 @@ import AvatarImg from "../assets/avatar.jpg";
 import { NavLink } from "react-router-dom";
 
 function Header() {
+  const MenuItem = [
+    { name: "Collections", href: "/" },
+    { name: "Men", href: "men" },
+    { name: "Women", href: "woman" },
+    { name: "About", href: "about" },
+    { name: "Contact", href: "contact" },
+  ];
   return (
-    <header className="flex py-7 items-center">
+    <header className="flex py-6 items-center">
       <h1 className="font-logo text-3xl">sneakers</h1>
-      <nav className="flex gap-4 items-center text-Grayish-blue ml-14">
-        <NavLink
-          to="/"
-          end
-          className={({ isActive }) =>
-            isActive ? "text-black border-b-2 border-orange" : ""
-          }
-        >
-          Collections
-        </NavLink>
-        <NavLink className="hover:text-black">Men</NavLink>
-        <NavLink className="hover:text-black">Women</NavLink>
-        <NavLink to="about" className="hover:text-black">
-          About
-        </NavLink>
-        <NavLink className="hover:text-black">Contact</NavLink>
+      <nav className="flex box-border h-5 gap-4 items-center text-Grayish-blue ml-14">
+        {MenuItem.map((item) => (
+          <NavLink
+            key={item}
+            to={item.href}
+            className={({ isActive }) =>
+              isActive
+                ? "relative text-black after:block after:bg-orange after:w-[100%] after:h-1 after:absolute after:mt-8 after:transition-all duration-500"
+                : "transition-all duration-300"
+            }
+          >
+            {item.name}
+          </NavLink>
+        ))}
       </nav>
       <div className="flex gap-8 items-center ml-auto">
         <ShoppingCart size={22} className="fill-Grayish-blue" weight="bold" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'logo': 'Outfit',
-        'san': 'Kumbh Sans'
-      }, 
+        logo: "Outfit",
+        san: "Kumbh Sans",
+      },
       colors: {
-        'orange': 'hsl(26, 100%, 55%)',
-        'pale-orange': 'hsl(25, 100%, 94%)',
-        'Dark-grayish-blue': 'hsl(219, 9%, 45%)',
-        'Grayish-blue': 'hsl(220, 14%, 75%)',
-        'Light-grayish-blue': 'hsl(223, 64%, 98%)',
-        'white': 'hsl(0, 0%, 100%)',
-      }
+        orange: "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25, 100%, 94%)",
+        "Dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "Grayish-blue": "hsl(220, 14%, 75%)",
+        "Light-grayish-blue": "hsl(223, 64%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      boxShadow: {
+        "3xl": "5px 10px 4px rgba(0, 0, 0, 0.1)",
+      },
     },
   },
   plugins: [],
-}
-
+};
